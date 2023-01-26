e53102170b2a47d0eee5bbbf442429158d4f5426
ilvdrskn <vladalievaliev@gmail.com>
2023-01-26 15:27:43 +0400

    made dropdownlist

--- code.js
+++ code.js
@@ -1,8 +1,18 @@
 window.onload = function() {
     document.getElementsByClassName("menu-svg")[0].onclick = function() {
         document.getElementsByClassName("modal-menu")[0].classList.remove("closeModal");
+        document.getElementsByClassName("main-container")[0].classList.add("hide");
         document.getElementsByClassName("svg-close")[0].onclick = function() {
             document.getElementsByClassName("modal-menu")[0].classList.add("closeModal");
+            document.getElementsByClassName("main-container")[0].classList.remove("hide");
+        }
+    }
+    
+    var items = document.getElementsByClassName("item");
+    for (var i = 0; i < items.length; i ++) {
+        items[i].onclick = function() {
+            this.classList.toggle("hiden");
+
         }
     }
 }
